0b23b0f67a03d673379876412cdccfb1ea751957
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-02-16 17:53:46 +0000

    first step- data from array in state

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
+import React,{Component} from 'react'
 
-function App() {
-  return (
-    <div>
-      App fetch
-    </div>
-  );
+class App extends Component {
+  state = { 
+    words:  [
+      {
+        "id": 1,
+        "en": "dog",
+        "fr": "chien"
+      },
+      {
+        "id": 2,
+        "en": "cat",
+        "fr": "chat"
+      },
+      {
+        "id": 3,
+        "en": "boar",
+        "fr": "sanglier"
+      }
+    ]
+   } 
+  render() { 
+    const words = this.state.words.map(word => (
+      <div key={word.id}>
+        <h2>{word.en}</h2>
+        <h1>{word.fr}</h1>
+      </div>
+    ));
+    return (
+      <div>{words}</div>
+    );
+  }
 }
-
+ 
 export default App;
